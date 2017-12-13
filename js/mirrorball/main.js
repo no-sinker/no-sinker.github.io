682c2dde7050b3e6d44fe1d0b7029162948c1c47
ponkore <ponkore@gmail.com>
2017-12-14 01:11:55 +0900

    one colored cube removed. environment picture updated.

--- js/mirrorball/main.js
+++ js/mirrorball/main.js
@@ -1,7 +1,22 @@
+// based on http://irukanobox.blogspot.jp/2016/08/threejs_13.html, http://matorel.com/archives/723
+
 // キューブマップ用のテクスチャ作成
+// var cubeTexture = new THREE.CubeTextureLoader()
+//     .setPath( './image/sunflower/' )
+//     .load( [ 'sunflower_er_left.png',
+//              'sunflower_er_right.png',
+//              'sunflower_er_up.png',
+//              'sunflower_er_down.png',
+//              'sunflower_er_front.png',
+//              'sunflower_er_back.png' ] );
 var cubeTexture = new THREE.CubeTextureLoader()
-		.setPath( './image/sunflower/' )
-		.load( [ 'sunflower_er_left.png', 'sunflower_er_right.png', 'sunflower_er_up.png', 'sunflower_er_down.png', 'sunflower_er_front.png', 'sunflower_er_back.png' ] );
+    .setPath( './image/clouds1/' )
+    .load( [ 'clouds1_south.png',
+             'clouds1_west.png',
+             'clouds1_up.png',
+             'clouds1_down.png',
+             'clouds1_north.png',
+             'clouds1_east.png' ] );
 cubeTexture.format = THREE.RGBFormat;
 
 // シーンの作成
@@ -30,17 +45,17 @@ var geometry = new THREE.SphereGeometry( 200, 64, 32 );
 var fIndices = [ 'a', 'b', 'c' ];
 var color, f, vIndex, hue;
 for ( var i = 0; i < geometry.faces.length; i++ ) {
-	f = geometry.faces[ i ];
+        f = geometry.faces[ i ];
 
-	if( i % 2 == 0 ) hue = Math.random();
-	for( var j = 0; j < 3; j ++ ) {
-		vIndex = f[ fIndices[ j ] ];
+        if( i % 2 == 0 ) hue = Math.random();
+        for( var j = 0; j < 3; j ++ ) {
+                vIndex = f[ fIndices[ j ] ];
 
-		color = new THREE.Color( 0xffffff );
-		color.setHSL( hue, 1.0, 0.7 );
+                color = new THREE.Color( 0xffffff );
+                color.setHSL( hue, 1.0, 0.7 );
 
-		f.vertexColors[ j ] = color;
-	}
+                f.vertexColors[ j ] = color;
+        }
 }
 //geometry.elementsNeedUpdate = true;
 
@@ -49,27 +64,27 @@ for ( var i = 0; i < geometry.faces.length; i++ ) {
 // 鏡のパネルが並んでいるようにTHREE.FlatShadingを使う
 var material1 = new THREE.MeshStandardMaterial( { color: 0xffffff, envMap: cubeTexture, shading: THREE.FlatShading, roughness: 0.1, metalness: 0.9} );
 // vertexColors: THREE.VertexColorsを設定して色の変更を反映させる
-var material2 = new THREE.MeshStandardMaterial( { color: 0xffffff, envMap: cubeTexture, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, roughness: 0.1, metalness: 0.9} );
+// var material2 = new THREE.MeshStandardMaterial( { color: 0xffffff, envMap: cubeTexture, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, roughness: 0.1, metalness: 0.9} );
 
 // オブジェクトの作成
 var mball1 = new THREE.Mesh( geometry, material1 );
-mball1.position.x = -250;
-var mball2 = new THREE.Mesh( geometry, material2 );
-mball2.position.x = 250;
+// mball1.position.x = -250;
+// var mball2 = new THREE.Mesh( geometry, material2 );
+// mball2.position.x = 250;
 
 // オブジェクトをシーンへ追加
 scene.add( mball1 );
-scene.add( mball2 );
+// scene.add( mball2 );
 
 // OrbitControlsインスタンス作成
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 function render(){
-	requestAnimationFrame( render );
+        requestAnimationFrame( render );
 
-	// レンダリング
-	renderer.render(scene,camera);
+        // レンダリング
+        renderer.render(scene,camera);
 
-	controls.update();
+        controls.update();
 }
 render();
